d52862c7a3b2a7a574fe7979cf7406cb2204c84d
anthonymramos8 <159965363+anthonymramos8@users.noreply.github.com>
2024-03-26 15:15:59 -0400

    Interface work
    Some work on abbreviating the view and template for Candy Land

--- candyland/views.py
+++ candyland/views.py
@@ -1,19 +1,12 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 
-# Create your views here.
 def candyland_view(request):
     # AJAX requests
     if (request.method == 'POST' and request.headers.get('x-requested-with') == 'XMLHttpRequest'):
-        try:
-            input = request.POST.get('input')
-            print(input)
-            return JsonResponse({'value': input})
-        except Exception as e:
-            print("Error processing AJAX request:", e)
-            return JsonResponse({'error': 'An error occurred while processing your request'}, status=500)
-    elif (request.method == 'GET'):
-        return JsonResponse({'message': 'GET request received'})
+        input = request.POST.get('input')
+        print(input)
+        return JsonResponse({'value': input})
     
     # Initial HTTP request; setup, page render
     else:
