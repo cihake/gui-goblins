6abadecf7c0716d91594de3c6f7559435fa825e7
anthonymramos8 <159965363+anthonymramos8@users.noreply.github.com>
2024-04-12 20:47:33 -0400

    Moved Monopoly to AJAX
    Converted direct variable changed in JavaScript to AJAX request/response and Player model. Fixed URLs for AJAX.

--- monopoly/models/player.py
+++ monopoly/models/player.py
@@ -4,6 +4,7 @@ A player of the game. They have an ordinal (which player they are),
 the space that their piece is on, and a money total.
 """
 class Player(models.Model):
+    game_key = models.UUIDField(unique=True, null=True)
     ordinal = models.IntegerField(default=-1)
     space = models.IntegerField(default=0)
     money = models.IntegerField(default=0)
