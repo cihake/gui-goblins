79d24cb0c94ab32dfd0e4bbfcd2edd3319cd91dc
anthonymramos8 <159965363+anthonymramos8@users.noreply.github.com>
2024-04-23 11:42:30 -0400

    Added a build button to Catan
    Added a build button to Settlers of Catan, with a corresponding game flag

--- catan/models/game.py
+++ catan/models/game.py
@@ -7,3 +7,5 @@ class Game(models.Model):
     game_key = models.UUIDField(unique=True)
     turn = models.IntegerField(default=1)
     number_players = models.IntegerField(default=1)
+    # Flag for building; 0 = no build, 1 = settlement
+    build_flag = models.IntegerField(default=0)
