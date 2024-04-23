0c2df6ef8915a71b2924d4a0b49f2c7eb1b56d1a
anthonymramos8 <159965363+anthonymramos8@users.noreply.github.com>
2024-04-23 14:24:40 -0400

    Catan game setup
    Added initial building placement (with tests) for Settlers of Catan, plus some feedback

--- catan/models/game.py
+++ catan/models/game.py
@@ -7,5 +7,7 @@ class Game(models.Model):
     game_key = models.UUIDField(unique=True)
     turn = models.IntegerField(default=1)
     number_players = models.IntegerField(default=1)
+    # Flag for setup; also indicates the "turn" of setup
+    setup_flag = models.IntegerField(default=1)
     # Flag for building; 0 = no build, 1 = settlement
     build_flag = models.IntegerField(default=0)
