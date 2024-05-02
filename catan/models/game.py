ae773eb7433bd6edb2537c766481a8c907003e6c
anthonymramos8 <159965363+anthonymramos8@users.noreply.github.com>
2024-05-02 18:54:30 -0400

    Adapted setup to multiplayer
    Adapted the setup phase for Settlers of Catan to multiplayer, with players placing settlements one at a time. Updated the tests.

--- catan/models/game.py
+++ catan/models/game.py
@@ -3,8 +3,8 @@ from .player import Player
 """
 Game overhead. It stores the players, along with flags for turn progression
 and multi-step interaction.
-The setup flag can refer to the player setting up, and the build flag can refer
-to multiple kinds of building; 0 = no build, 1 = settlement, 2 = road start, 3 = road end
+The setup flag is for the setup phase of the game, and the build flag can refer to multiple
+kinds of building; 0 = no build, 1 = settlement, 2 = road start, 3 = road end, 4 = city
 """
 class Game(models.Model):
     game_key = models.UUIDField(unique=True)
